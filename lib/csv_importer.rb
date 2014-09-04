0a31b94d8796c56741dc98fa6c2c1deed63ecbd9
Nestor Castillo <nestor.castillo@izea.com>
2014-09-03 22:32:33 -0400

    Dojo progress

--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -2,4 +2,28 @@ require 'csv'
 
 class CSVImporter
 
+  attr_reader :filename, :list_of_names
+
+  def initialize(filename)
+    @filename = filename
+    @list_of_names = []
+  end
+
+  def parse_from_csv
+    @list_of_names = CSV.read("./lib/#{@filename}")[1..-1]
+  end
+
+  def total_count(stuff=nil)
+    if stuff != nil
+      count = []
+      @list_of_names.each do |name|
+        if name.include? stuff
+          count.push(name)
+        end
+      end
+    else
+      count.push(@list_of_names)
+    end
+    count.count
+  end
 end
--- spec/csv_importer_spec.rb
+++ spec/csv_importer_spec.rb
@@ -1,14 +1,28 @@
 require './lib/csv_importer'
 
 describe CSVImporter do
+  let(:importer) {CSVImporter.new("test_names.csv")}
   it "accepts a file name" do
-    importer = CSVImporter.new("test_names.csv")
+
     expect(importer.filename).to eq "test_names.csv"
   end
 
-  # it "has an empty list of names" do
-  #   importer = CSVImporter.new("test_names.csv")
+  it "has an empty list of names" do
 
-  #   expect(importer.list_of_names).to eq []
-  # end
+    expect(importer.list_of_names).to eq []
+  end
+
+  it "parses from csv" do
+    importer.parse_from_csv
+
+    expect(importer.list_of_names).not_to be_empty
+  end
+  it "count the number of men in the csv" do
+    importer.parse_from_csv
+    expect(importer.total_count).to eq(17)
+  end
+
+  it "should count the amount of males" do
+    expect(importer.total_count("m")).to eq(10)
+  end
 end
